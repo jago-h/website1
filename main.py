895b14b39691774a07f58e89dfdeae7055000221
Jagoda Hanuszewicz <jagoda.hanuszewicz@gmail.com>
2024-08-06 14:28:44 +0700

    add Upload images option in scanner.html

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, request
+from flask import render_template
 
 app = Flask(__name__)
 app.secret_key = 'document_scanner_app'
 
 @app.route('/')
-def index():
-    return "Welcome to the text scanner app!"
+def scandoc():
+    return render_template('scanner.html')
+
+@app.route('/about')
+def about():
+    return "Here will come some information about used sources for this website, such as udemy course business-card-reader-app :)"
 
 if __name__ == "__main__":
     app.run(debug=True)
